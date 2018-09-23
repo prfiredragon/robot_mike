905266016a64ba3ea5468e018c76ef0a7a1647ed
Roberto Rodriguez Cintron <prfiredragon@gmail.com>
2018-09-23 02:03:26 -0400

    add encoder

--- src/robot_mike/arduino/Robot_Mike_Arduino/Robot_Mike_Arduino.ino
+++ src/robot_mike/arduino/Robot_Mike_Arduino/Robot_Mike_Arduino.ino
@@ -16,17 +16,37 @@
 #include <std_msgs/String.h>
 #include <std_msgs/Int16.h>
 #include <std_msgs/Float32.h>
+//Make a pin into an interrupt
+#include <PinChangeInterrupt.h>
 
 #define LDir 10
 #define LPWM 11
 #define RDir 12
 #define RPWM 9
 
+///////////////////////////////////////////////////////////////
+//Encoder pins definition
+
+// Left encoder
+
+#define Left_Encoder_PinA 2
+#define Left_Encoder_PinB 3
+
+int Left_Encoder_Ticks = 0;
+volatile bool LeftEncoderBSet;
+
+//Right Encoder
+
+#define Right_Encoder_PinA 4
+#define Right_Encoder_PinB 8
+int Right_Encoder_Ticks = 0;
+volatile bool RightEncoderBSet;
+
+
+
 
 ros::NodeHandle nh;
 
-std_msgs::String msg_debug;
-ros::Publisher debug_pub("arduino_debug", &msg_debug);
 std_msgs::Int16 msg_lwheel;
 std_msgs::Int16 msg_rwheel;
 
@@ -49,9 +69,6 @@ long time=0;
 //////////////////////////////////////////////////////////////////////
 
 void lfwd(int speed=255) {
-	sprintf(debug_str, "lfwd %d", speed);
-	msg_debug.data = debug_str;
-	debug_pub.publish( &msg_debug );
 
 
 
@@ -62,9 +79,6 @@ void lfwd(int speed=255) {
 
 
 void lrev(int speed=255) {
-	sprintf(debug_str, "lrev %d", speed);
-	msg_debug.data = debug_str;
-	debug_pub.publish( &msg_debug );
 
 
       analogWrite(LPWM, constrain(speed, 0, 255 ));
@@ -73,9 +87,6 @@ void lrev(int speed=255) {
 }
 
 void lcoast() {
-	sprintf(debug_str, "lcoast");
-	msg_debug.data = debug_str;
-	debug_pub.publish( &msg_debug );
 
 
       analogWrite(LPWM, 0);
@@ -83,9 +94,6 @@ void lcoast() {
 }
 
 void lbrake() {
-	sprintf(debug_str, "lbrake");
-	msg_debug.data = debug_str;
-	debug_pub.publish( &msg_debug );
 
 
       analogWrite(LPWM, 0);
@@ -99,9 +107,6 @@ void lbrake() {
 //////////////////////////////////////////////////////////////////////
 
 void rfwd( int speed=255) {
-	sprintf(debug_str, "rfwd %d", speed);
-	msg_debug.data = debug_str;
-	debug_pub.publish( &msg_debug );
 
 
       analogWrite(RPWM, constrain(speed, 0, 255 ));
@@ -110,9 +115,6 @@ void rfwd( int speed=255) {
 }
 
 void rrev(int speed=255) {
-	sprintf(debug_str, "rrev %d", speed);
-	msg_debug.data = debug_str;
-	debug_pub.publish( &msg_debug );
 
 
       analogWrite(RPWM, constrain(speed, 0, 255 ));
@@ -122,9 +124,6 @@ void rrev(int speed=255) {
 
 
 void rcoast() {
-	sprintf(debug_str, "rcoast");
-	msg_debug.data = debug_str;
-	debug_pub.publish( &msg_debug );
 
 
       analogWrite(RPWM, 0);
@@ -132,9 +131,6 @@ void rcoast() {
 }
 
 void rbrake() {
-	sprintf(debug_str, "rbrake");
-	msg_debug.data = debug_str;
-	debug_pub.publish( &msg_debug );
 
 
       analogWrite(RPWM, 0);
@@ -149,9 +145,6 @@ void rbrake() {
 
 
 void RMotorCallBack( const std_msgs::Float32& motor_msg) {
-	sprintf(debug_str, "LMotorCallback %0.3f", motor_msg.data);
-	msg_debug.data = debug_str;
-	debug_pub.publish( &msg_debug );
 
     if (motor_msg.data > 255 || motor_msg.data < -255) {
     	rbrake();
@@ -181,6 +174,28 @@ void LMotorCallBack( const std_msgs::Float32& motor_msg) {
 ros::Subscriber<std_msgs::Float32> rmotor_sub("rmotor_cmd", &RMotorCallBack);
 ros::Subscriber<std_msgs::Float32> lmotor_sub("lmotor_cmd", &LMotorCallBack);
 
+//SetupEncoders() Definition
+
+void SetupEncoders()
+{
+  // Quadrature encoders
+  // Left encoder
+  pinMode(Left_Encoder_PinA, INPUT_PULLUP);      // sets pin A as input  
+  pinMode(Left_Encoder_PinB, INPUT);      // sets pin B as input
+  //Attaching interrupt in Left_Enc_PinA.
+  //attachInterrupt(digitalPinToInterrupt(Left_Encoder_PinA), do_Left_Encoder, RISING);
+  attachPinChangeInterrupt(digitalPinToPinChangeInterrupt(Left_Encoder_PinA),do_Left_Encoder, RISING);
+  
+
+  // Right encoder
+  pinMode(Right_Encoder_PinA, INPUT_PULLUP);      // sets pin A as input
+  pinMode(Right_Encoder_PinB, INPUT);      // sets pin B as input
+  //Attaching interrupt in Right_Enc_PinA.
+  //attachInterrupt(digitalPinToInterrupt(Right_Encoder_PinA), do_Right_Encoder, RISING); 
+  attachPinChangeInterrupt(digitalPinToPinChangeInterrupt(Right_Encoder_PinA),do_Right_Encoder, RISING);
+
+}
+
 void setup(){  
   pinMode(10,  OUTPUT);                  //direction left
   pinMode(11,  OUTPUT);                  //PWM left
@@ -190,23 +205,50 @@ void setup(){
  
   nh.initNode();
   nh.getHardware()->setBaud(57600);
-  nh.advertise(debug_pub);
   nh.advertise(lwheel_pub);
   nh.advertise(rwheel_pub);
   nh.subscribe(rmotor_sub);
   nh.subscribe(lmotor_sub);
+
+   //Setup Encoders
+  SetupEncoders();
 }
 
 void loop(){
 
-    	msg_debug.data = debug_str;
-    	debug_pub.publish( &msg_debug );
-  //msg_lwheel.data = lcoder;
-  //msg_rwheel.data = rcoder;
+  msg_lwheel.data = Left_Encoder_Ticks;
+  msg_rwheel.data = Right_Encoder_Ticks;
 
   lwheel_pub.publish( &msg_lwheel );
   rwheel_pub.publish( &msg_rwheel );
+
+
   
   nh.spinOnce();
   delay(10);
+}
+
+
+
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//do_Left_Encoder() Definitions
+void do_Left_Encoder()
+{
+   // Test transition; since the interrupt will only fire on 'rising' we don't need to read pin A
+  LeftEncoderBSet = digitalRead(Left_Encoder_PinB);   // read the input pin
+  Left_Encoder_Ticks += LeftEncoderBSet ? -1 : +1;
+   
+}
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//do_Right_Encoder() Definitions
+
+void do_Right_Encoder()
+{
+  
+  RightEncoderBSet = digitalRead(Right_Encoder_PinB);   // read the input pin
+  Right_Encoder_Ticks -= RightEncoderBSet ? -1 : +1;
+ 
+ 
+  
 }
